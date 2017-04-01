c2ba9d2315767bf613e85257c3ea1f42242cc4ee
LaurentiuCalin <durnea.calin@gmail.com>
2017-04-01 19:32:14 +0200

    -

--- functions/sendEmail/SendEmailConfirmation.php
+++ functions/sendEmail/SendEmailConfirmation.php
@@ -5,9 +5,9 @@ function emailConfirmation($sUserEmail, $emailToken)
     $to=$sUserEmail;
     $subject="Activate your account on AirQuick ";
     $from = 'emailconfirmation@airquick.dk';
-    $body='This is the activation e-mail to your AirQuick account. Please Click On This <a href="confirm.php?et='.$emailToken.'">link</a>to activate your account.';
+    $body="This is the activation e-mail to your AirQuick account. Please Click On This <a href='confirm.php?et=$emailToken'>link</a>to activate your account.";
     $headers = "From:".$from;
-
+echo $body;
     if (mail($to,$subject,$body,$headers)) {
       	echo "An Activation Code Is Sent To You Check You Email";
     }else{
