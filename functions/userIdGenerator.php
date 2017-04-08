d972fe9c6ffbf63ace55b77e5134bbb58e6889de
daniellupascu <lupascu_daniel2000@yahoo.com>
2017-04-08 18:36:31 +0200

    Working on uploading files

--- functions/userIdGenerator.php
+++ functions/userIdGenerator.php
@@ -4,5 +4,18 @@ function generateId($userEmail){
     return hexdec( substr(sha1($userEmail), 0, 5) );
 }
 
+function generateRandomString(){
+
+    $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    $string = '';
+    $max = strlen($characters) - 1;
+    for ($i = 0; $i < 35; $i++) {
+        $string .= $characters[mt_rand(0, $max)];
+    }
+
+    return $string;
+}
+
 
 ?>
